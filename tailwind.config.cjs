1f1326b4d2f10bec8a77660f267616039dfe10c9
TaanDigital <dev@taandigital.com>
2025-05-31 22:22:59 -0600

    FT Astro new version
    Agregando cambios y estilos

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+// tailwind.config.cjs
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors: {
+          'sky-blue-light': '#A2D4F5',
+          'mint-green-soft': '#B3E6C1',
+          'pure-white': '#FFFFFF',
+          'gray-soft': '#D9D9D9',
+          'coral-vibrant': '#FF6B6B',
+        }
+      }
+    }
+  };
